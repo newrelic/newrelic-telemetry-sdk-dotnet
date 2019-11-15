b07f450a78802ca346e1b85405b4f072a7185686
Vu Tran <vuqtran88@gmail.com>
2019-11-15 14:16:52 -0800

    Adds backoff sequence test.

--- src/NewRelic.Telemetry.Tests/TelemetryClientTests.cs
+++ src/NewRelic.Telemetry.Tests/TelemetryClientTests.cs
@@ -3,11 +3,7 @@ using NewRelic.Telemetry.Client;
 using NewRelic.Telemetry.Spans;
 using NewRelic.Telemetry.Transport;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace NewRelic.Telemetry.Tests
@@ -15,27 +11,43 @@ namespace NewRelic.Telemetry.Tests
     class TelemetryClientTests
     {
         [Test]
-        async public Task TestTelemetryClient_Retry()
+        async public Task TestTelemetryClient_RetryBackoffSequence()
         {
-            var calls = 0;
+            var expectedBackoffSequenceFromTestRun = new List<int>() 
+            {
+                5000,
+                10000,
+                20000,
+                40000,
+                80000,
+                80000,
+                80000,
+                80000
+            };
+
+            var actualBackoffSequenceFromTestRun = new List<int>();
+
+            var mockDelayer = new Mock<IAsyncDelayer>();
+            mockDelayer.Setup(x => x.Delay(It.IsAny<int>()))
+                .Returns((int milliSecondsDelay) =>
+                {
+                    actualBackoffSequenceFromTestRun.Add(milliSecondsDelay);
+                    return Task.FromResult(0);
+                });
+
             var mockSpanBatchSender = new Mock<ISpanBatchSender>();
             mockSpanBatchSender.Setup(x => x.SendDataAsync(It.IsAny<SpanBatch>()))
-            .Callback(() =>
-            {
-                calls++;
-            }).Returns(() => {
-                if (calls == 3) 
+                .Returns(() =>
                 {
-                    return Task.FromResult(new Response(true, System.Net.HttpStatusCode.OK));
-                }
-                return Task.FromResult(new Response(true, System.Net.HttpStatusCode.RequestTimeout));
-            });
+                    return Task.FromResult(new Response(true, System.Net.HttpStatusCode.RequestTimeout));
+                });
 
-            var client = new TelemetryClient(mockSpanBatchSender.Object);
+            var client = new TelemetryClient(mockSpanBatchSender.Object, mockDelayer.Object);
+
             await client.SendBatchAsync(It.IsAny<SpanBatch>());
 
-            mockSpanBatchSender.Verify(x => x.SendDataAsync(It.IsAny<SpanBatch>()), Times.Exactly(3));
-
+            mockSpanBatchSender.Verify(x => x.SendDataAsync(It.IsAny<SpanBatch>()), Times.Exactly(9));
+            CollectionAssert.AreEqual(expectedBackoffSequenceFromTestRun, actualBackoffSequenceFromTestRun);
             return;
         }
     }
--- src/NewRelic.Telemetry/Client/TelemetryClient.cs
+++ src/NewRelic.Telemetry/Client/TelemetryClient.cs
@@ -2,20 +2,25 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
-using System.Threading;
 
 namespace NewRelic.Telemetry.Client
 {
     public class TelemetryClient
     {
-        ISpanBatchSender _spanBatchSender;
+        private ISpanBatchSender _spanBatchSender;
+        private readonly IAsyncDelayer _asyncDelayer;
         private const int BACKOFF_FACTOR = 5; // In seconds.
         private const int BACKOFF_MAX = 80; // In seconds. 
         private const int MAX_RETRIES = 8;
 
-        public TelemetryClient(ISpanBatchSender spanBatchSender)
+        public TelemetryClient(ISpanBatchSender spanBatchSender) : this(spanBatchSender, AsyncDelayer.Instance)
+        {
+        }
+
+        internal TelemetryClient(ISpanBatchSender spanBatchSender, IAsyncDelayer asyncDelayer)
         {
             _spanBatchSender = spanBatchSender;
+            _asyncDelayer = asyncDelayer;
         }
 
         public async Task SendBatchAsync(SpanBatch spanBatch) 
@@ -50,7 +55,7 @@ namespace NewRelic.Telemetry.Client
                     //handle 429 error according to the spec.
                     break;
                 default:
-                    //retry
+                    Logging.LogError($@"Response from New Relic ingest API: code: {response.StatusCode}");
                     break;
             }
         }
@@ -65,8 +70,32 @@ namespace NewRelic.Telemetry.Client
 
             var waitTime = (int) Math.Min(BACKOFF_MAX, BACKOFF_FACTOR * Math.Pow(2, retryNum - 1)) * 1000;
 
-            await Task.Delay(waitTime);
+            await _asyncDelayer.Delay(waitTime);
             await SendBatchAsyncInternal(spanBatch, retryNum);
+            return;
+        }
+    }
+
+    internal interface IAsyncDelayer
+    {
+        Task Delay(int millisecondsDelay);
+    }
+
+    internal class AsyncDelayer : IAsyncDelayer
+    {
+        private static readonly AsyncDelayer _singleton = new AsyncDelayer();
+
+        public static AsyncDelayer Instance
+        {
+            get
+            {
+                return _singleton;
+            }
+        }
+
+        public Task Delay(int millisecondsDelay)
+        {
+            return Task.Delay(millisecondsDelay);
         }
     }
 }
