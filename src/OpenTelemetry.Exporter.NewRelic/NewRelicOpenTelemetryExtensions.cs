5193a82ac44ab9785d697bac1d9ce330515cd9f3
Jason Feingold <jfeingold@newrelic.com>
2019-11-25 17:24:55 -0800

    Exension Functions

---
+++ src/OpenTelemetry.Exporter.NewRelic/NewRelicOpenTelemetryExtensions.cs
@@ -0,0 +1,31 @@
+﻿using NewRelic.Telemetry;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Trace.Configuration;
+
+namespace OpenTelemetry.Exporter.NewRelic
+{
+    public static class NewRelicOpenTelemetryExtensions
+    {
+        public static TracerBuilder UseNewRelic(this TracerBuilder builder, TelemetryConfiguration config, ILoggerFactory loggerFactory)
+        {
+            builder.AddProcessorPipeline(c => c.SetExporter(new NewRelicTraceExporter(config, loggerFactory)));
+            return builder;
+        }
+
+        public static TracerBuilder UseNewRelic(this TracerBuilder builder, TelemetryConfiguration config)
+        {
+            return UseNewRelic(builder, config, null);
+        }
+
+        public static TracerBuilder UseNewRelic(this TracerBuilder builder, string apiKey)
+        {
+            return UseNewRelic(builder, apiKey, null);
+        }
+
+        public static TracerBuilder UseNewRelic(this TracerBuilder builder, string apiKey, ILoggerFactory loggerFactory)
+        {
+            builder.AddProcessorPipeline(c => c.SetExporter(new NewRelicTraceExporter(new TelemetryConfiguration().WithAPIKey(apiKey),loggerFactory)));
+            return builder;
+        }
+    }
+}
