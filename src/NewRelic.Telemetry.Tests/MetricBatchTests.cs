fa7648efb82a2814ebdb3798a04f75300f122944
Lynice Spangler <20267975+lspangler584@users.noreply.github.com>
2019-12-23 16:01:12 -0800

    adds MetricsTests and MetricBatchTests

--- src/NewRelic.Telemetry.Tests/MetricBatchTests.cs
+++ src/NewRelic.Telemetry.Tests/MetricBatchTests.cs
@@ -9,14 +9,102 @@ namespace NewRelic.Telemetry.Tests
     public class MetricBatchTests
     {
         [Test]
-        public void MetricBatchWithCommonTimestamp()
+        public void MetricBatchWithCommonTimestampAndNoMetrics()
         {
             var timestamp = DateTime.UtcNow;
             var metricBatch = MetricBatchBuilder.Create()
                 .WithTimestamp(timestamp)
                 .Build();
             
-            Assert.AreEqual(DateTimeExtensions.ToUnixTimeMilliseconds(timestamp), metricBatch.CommonProperties.Timestamp);
+            Assert.AreEqual(DateTimeExtensions.ToUnixTimeMilliseconds(timestamp), metricBatch.CommonProperties.Timestamp, $"MetricBatch CommonProperties [Timestamp] - expected: {timestamp}, actual: {metricBatch.CommonProperties.Timestamp}");
+            Assert.IsNull(metricBatch.Metrics, $"MetricBatch Metrics - expected: null, actual: not null");
         }
+
+        [Test]
+        public void MetricBatchWithCommonPropertiesAndMetrics()
+        {
+            var timestamp = DateTime.UtcNow;
+            var interval = 125L;
+            var commonAttrs = new Dictionary<string, object>() { { "attr1Key", "attr1Value" } };
+            var countValue = 88d;
+            var gaugeValue = 213d;
+
+            var metricBatch = MetricBatchBuilder.Create()
+                .WithTimestamp(timestamp)
+                .WithIntervalMs(interval)
+                .WithAttributes(commonAttrs)
+                .WithMetric(MetricBuilder.CreateCountMetric("CountMetric")
+                    .WithValue(countValue)
+                    .Build())
+                .WithMetric(MetricBuilder.CreateGaugeMetric("GaugeMetric")
+                    .WithValue(gaugeValue)
+                    .Build())
+                .Build();
+
+            Assert.AreEqual(DateTimeExtensions.ToUnixTimeMilliseconds(timestamp), metricBatch.CommonProperties.Timestamp, $"MetricBatch CommonProperties [Timestamp] - expected: {timestamp}, actual: {metricBatch.CommonProperties.Timestamp}");
+            Assert.AreEqual(interval, metricBatch.CommonProperties.IntervalMs, $"MetricBatch CommonProperties [IntervalMs] - expected: {interval}, actual: {metricBatch.CommonProperties.IntervalMs}");
+
+            Assert.AreEqual(2, metricBatch.Metrics.Count, $"MetricBatch Metrics count - expected: 2, actual: {metricBatch.Metrics.Count}");
+            Assert.AreEqual("count", metricBatch.Metrics[0].Type, $"MetricBatch Metrics[0].Type - expected: count, actual: {metricBatch.Metrics[0].Type}");
+            Assert.AreEqual("gauge", metricBatch.Metrics[1].Type, $"MetricBatch Metrics[1].Type - expected: gauge, actual: {metricBatch.Metrics[1].Type}");
+        }
+
+        [Test]
+        public void MetricBatchAllowsCommonAndSpecificSameNamedFields()
+        {
+            var commonTimestamp = DateTime.UtcNow;
+            var commonInterval = 125L;
+
+            var metricTimestamp = DateTime.UtcNow + TimeSpan.FromSeconds(60);
+            var metricInterval = 312L;
+
+            var countValue = 88d;
+
+            var metricBatch = MetricBatchBuilder.Create()
+                .WithTimestamp(commonTimestamp)
+                .WithIntervalMs(commonInterval)
+                .WithAttribute("Attr1Key", "comAttr1Value")
+                .WithMetric(MetricBuilder.CreateCountMetric("CountMetric")
+                    .WithTimestamp(metricTimestamp)
+                    .WithIntervalMs(metricInterval)
+                    .WithAttribute("Attr1Key", "metAttr1Value")
+                    .WithValue(countValue)
+                    .Build())
+                .Build();
+
+            Assert.AreEqual(DateTimeExtensions.ToUnixTimeMilliseconds(commonTimestamp), metricBatch.CommonProperties.Timestamp, $"MetricBatch CommonProperties [Timestamp] - expected: {commonTimestamp}, actual: {metricBatch.CommonProperties.Timestamp}");
+            Assert.AreEqual(commonInterval, metricBatch.CommonProperties.IntervalMs, $"MetricBatch CommonProperties [IntervalMs] - expected: {commonInterval}, actual: {metricBatch.CommonProperties.IntervalMs}");
+            Assert.AreEqual("comAttr1Value", metricBatch.CommonProperties.Attributes["Attr1Key"], $"MetricBatch CommonProperties Attributes value - expected: comAttr1Value, actual: {metricBatch.CommonProperties.Attributes["Attr1Key"]}");
+
+            Assert.AreEqual(DateTimeExtensions.ToUnixTimeMilliseconds(metricTimestamp), metricBatch.Metrics[0].Timestamp, $"MetricBatch Metric [Timestamp] - expected: {DateTimeExtensions.ToUnixTimeMilliseconds(metricTimestamp)}, actual: {metricBatch.Metrics[0].Timestamp}");
+            Assert.AreEqual(metricInterval, metricBatch.Metrics[0].IntervalMs, $"MetricBatch Metric [IntervalMs] - expected: {metricInterval}, actual: {metricBatch.Metrics[0].IntervalMs}");
+            Assert.AreEqual("metAttr1Value", metricBatch.Metrics[0].Attributes["Attr1Key"], $"MetricBatch Metric Attributes value - expected: metAttr1Value, actual: {metricBatch.Metrics[0].Attributes["Attr1Key"]}");
+        }
+
+        [Test]
+        public void MetricBatchWithNoCommonElement()
+        {
+            var timestamp = DateTime.UtcNow;
+            var interval = 125L;
+            var gaugeValue = 213d;
+            var summaryValue = new MetricSummaryValue() { Count = 10d, Sum = 64, Min = 3, Max = 15 };
+
+            var metricBatch = MetricBatchBuilder.Create()
+                .WithMetric(MetricBuilder.CreateSummaryMetric("SummaryMetric")
+                    .WithTimestamp(timestamp)
+                    .WithIntervalMs(interval)
+                    .WithValue(summaryValue)
+                    .Build())
+                .WithMetric(MetricBuilder.CreateGaugeMetric("GaugeMetric")
+                    .WithTimestamp(timestamp)
+                    .WithIntervalMs(interval)
+                    .WithValue(gaugeValue)
+                    .Build())
+                .Build();
+
+            Assert.AreEqual(2, metricBatch.Metrics.Count, $"MetricBatch Metrics count - expected: 2, actual: {metricBatch.Metrics.Count}");
+            Assert.IsNull(metricBatch.CommonProperties, $"MetricBatch CommonProperties - expected: null, actual: not null");
+        }
+
     }
 }
--- src/NewRelic.Telemetry.Tests/MetricTests.cs
+++ src/NewRelic.Telemetry.Tests/MetricTests.cs
@@ -35,59 +35,61 @@ namespace NewRelic.Telemetry.Tests
             Assert.AreEqual("attrValue", metric.Attributes["attrKey"]);
         }
 
-        //[Test]
-        //public void BuildGaugeMetric()
-        //{
-        //    var timestamp = DateTime.UtcNow;
-        //    var interval = 33L;
-        //    var value = 87;
+        [Test]
+        public void BuildGaugeMetric()
+        {
+            var timestamp = DateTime.UtcNow;
+            var interval = 33L;
+            var value = 87;
 
-        //    var attributes = new Dictionary<string, object>
-        //    { { "attrKey", "attrValue" } };
+            var attributes = new Dictionary<string, object>
+            { { "attrKey", "attrValue" } };
 
-        //    var metricBuilder = MetricBuilder.Create("metricname", "gauge")
-        //        .WithValue(value)
-        //        .WithTimestamp(timestamp)
-        //        .WithIntervalMs(interval)
-        //        .WithAttribute("adsfasdf", 12)
-        //        .WithAttributes(attributes);
+            var metricBuilder = MetricBuilder.CreateGaugeMetric("metricname")
+                .WithValue(value)
+                .WithTimestamp(timestamp)
+                .WithIntervalMs(interval)
+                .WithAttribute("adsfasdf", 12)
+                .WithAttributes(attributes);
 
-        //    var metric = metricBuilder.Build();
+            var metric = metricBuilder.Build();
 
-        //    Assert.AreEqual("metricname", metric.Name);
-        //    Assert.AreEqual("gauge", metric.MetricType);
-        //    Assert.AreEqual(value, ((GaugeMetric)metric).Value);
-        //    Assert.AreEqual(DateTimeExtensions.ToUnixTimeMilliseconds(timestamp), metric.Timestamp);
-        //    Assert.AreEqual(interval, metric.IntervalMs);
-        //    Assert.AreEqual("attrValue", metric.Attributes["attrKey"]);
-        //}
+            Assert.AreEqual("metricname", metric.Name);
+            Assert.AreEqual("gauge", metric.Type);
+            Assert.AreEqual(value, ((GaugeMetric)metric).Value);
+            Assert.AreEqual(DateTimeExtensions.ToUnixTimeMilliseconds(timestamp), metric.Timestamp);
+            Assert.AreEqual(interval, metric.IntervalMs);
+            Assert.AreEqual(12, metric.Attributes["adsfasdf"]);
+            Assert.AreEqual("attrValue", metric.Attributes["attrKey"]);
+        }
 
-        //[Test]
-        //public void BuildSummaryMetric()
-        //{
-        //    var timestamp = DateTime.UtcNow;
-        //    var interval = 33L;
-        //    var value = new MetricSummaryValue() { Count = 10d, Sum = 64, Min = 3, Max = 15 };
+        [Test]
+        public void BuildSummaryMetric()
+        {
+            var timestamp = DateTime.UtcNow;
+            var interval = 33L;
+            var value = new MetricSummaryValue() { Count = 10d, Sum = 64, Min = 3, Max = 15 };
 
-        //    var attributes = new Dictionary<string, object>
-        //    { { "attrKey", "attrValue" } };
+            var attributes = new Dictionary<string, object>
+            { { "attrKey", "attrValue" } };
 
-        //    var metricBuilder = MetricBuilder.Create("metricname", "summary")
-        //        .WithValue(value)
-        //        .WithTimestamp(timestamp)
-        //        .WithIntervalMs(interval)
-        //        .WithAttribute("adsfasdf", 12)
-        //        .WithAttributes(attributes);
+            var metricBuilder = MetricBuilder.CreateSummaryMetric("metricname")
+                .WithValue(value)
+                .WithTimestamp(timestamp)
+                .WithIntervalMs(interval)
+                .WithAttribute("adsfasdf", 12)
+                .WithAttributes(attributes);
 
-        //    var metric = metricBuilder.Build();
+            var metric = metricBuilder.Build();
 
-        //    Assert.AreEqual("metricname", metric.Name);
-        //    Assert.AreEqual("summary", metric.MetricType);
-        //    Assert.AreEqual(value, ((SummaryMetric)metric).Value);
-        //    Assert.AreEqual(DateTimeExtensions.ToUnixTimeMilliseconds(timestamp), metric.Timestamp);
-        //    Assert.AreEqual(interval, metric.IntervalMs);
-        //    Assert.AreEqual("attrValue", metric.Attributes["attrKey"]);
-        //}
+            Assert.AreEqual("metricname", metric.Name);
+            Assert.AreEqual("summary", metric.Type);
+            Assert.AreEqual(value, metric.Value);
+            Assert.AreEqual(DateTimeExtensions.ToUnixTimeMilliseconds(timestamp), metric.Timestamp);
+            Assert.AreEqual(interval, metric.IntervalMs);
+            Assert.AreEqual(12, metric.Attributes["adsfasdf"]);
+            Assert.AreEqual("attrValue", metric.Attributes["attrKey"]);
+        }
 
         [Test]
         public void ThrowExceptionIfNullName()
