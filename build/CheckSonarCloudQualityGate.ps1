204b11258e63255c3c92877fae3050c371784b74
Jason Feingold <jfeingold@newrelic.com>
2020-01-09 09:17:58 -0800

    Added check against Quality Gate for build

---
+++ build/CheckSonarCloudQualityGate.ps1
@@ -0,0 +1,44 @@
+# This script checks for Quality Gate status for a SonarQube Project
+[CmdletBinding()]
+Param(
+    # Define Sonar Token
+    [Parameter (Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [String] $SonarToken,
+
+    # Define SonarQube Server URI
+    [Parameter (Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [String] $SonarServerName,
+
+    # Define Project Key
+    [Parameter (Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [String] $SonarProjectKey,
+
+    # Define Project Key
+    [Parameter (Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [int] $PullRequestId
+    
+)
+
+$Token = [System.Text.Encoding]::UTF8.GetBytes($SonarToken + ":")
+$TokenInBase64 = [System.Convert]::ToBase64String($Token)
+ 
+$basicAuth = [string]::Format("Basic {0}", $TokenInBase64)
+$Headers = @{ Authorization = $basicAuth }
+
+$qryParams = @{ projectKey = $SonarProjectKey; pullRequest = $PullRequestId}
+
+# $QualityGateResult = Invoke-RestMethod -Method Get -Uri http://$SonarServerName/api/qualitygates/project_status?projectKey=($SonarProjectKey)&amp;pullRequest=62 -Headers $Headers
+$QualityGateResult = Invoke-RestMethod -Method Get -Uri http://$SonarServerName/api/qualitygates/project_status -Headers $Headers -Body $qryParams
+
+$QualityGateResult | ConvertTo-Json | Write-Host
+ 
+if ($QualityGateResult.projectStatus.status -eq "OK"){
+    Write-Host "Quality Gate Succeeded"
+}
+else{
+    throw "Quality gate failed. Please check and fix the issues by reviewing the same."
+}
