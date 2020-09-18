2fc29c572bb242d1a626b4331ec6192360bd136f
Alan West <3676547+alanwest@users.noreply.github.com>
2020-09-18 08:39:01 -0700

    Cleanup comments and reorder methods

--- src/OpenTelemetry.Exporter.NewRelic/NewRelicExporterHelperExtensions.cs
+++ src/OpenTelemetry.Exporter.NewRelic/NewRelicExporterHelperExtensions.cs
@@ -13,27 +13,27 @@ namespace OpenTelemetry.Trace
     public static class NewRelicExporterHelperExtensions
     {
         /// <summary>
-        /// Advanced Configuration the New Relic Data Exporter that is configured using an instance of TelemetryConfiguration and a Logger Factory.
+        /// Adds New Relic exporter to the TracerProvider.
         /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="config"></param>
-        /// <param name="loggerFactory">Logger Factory supported by Microsoft.Extensions.Logging.</param>
-        /// <returns></returns>
+        /// <param name="builder"><see cref="TracerProviderBuilder"/> builder to use.</param>
+        /// <param name="config">Exporter configuration options.</param>
+        /// <returns>The instance of <see cref="TracerProviderBuilder"/> to chain the calls.</returns>
+        public static TracerProviderBuilder UseNewRelic(this TracerProviderBuilder builder, TelemetryConfiguration config)
+        {
+            return UseNewRelic(builder, config, null);
+        }
+
+        /// <summary>
+        /// Adds New Relic exporter to the TracerProvider and enables the exporter to log to an ILogger.
+        /// </summary>
+        /// <param name="builder"><see cref="TracerProviderBuilder"/> builder to use.</param>
+        /// <param name="config">Exporter configuration options.</param>
+        /// <param name="loggerFactory">ILoggerFactory instance for creating an ILogger.</param>
+        /// <returns>The instance of <see cref="TracerProviderBuilder"/> to chain the calls.</returns>
         public static TracerProviderBuilder UseNewRelic(this TracerProviderBuilder builder, TelemetryConfiguration config, ILoggerFactory loggerFactory)
         {
             builder.AddProcessor(new BatchExportActivityProcessor(new NewRelicTraceExporter(config, loggerFactory)));
             return builder;
-        }
-
-        /// <summary>
-        /// Advanced Configuration of the New Relic Data Exporter providing an instance of TelemetryConfiguration settings without logging.
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="config"></param>
-        /// <returns></returns>
-        public static TracerProviderBuilder UseNewRelic(this TracerProviderBuilder builder, TelemetryConfiguration config)
-        {
-            return UseNewRelic(builder, config, null);
         }
     }
 }
