82a3cdf789c6fb1f1c3807c867bd5d9c4ff36fc0
Alex Hemsath <ahemsath@newrelic.com>
2020-10-30 11:55:37 -0700

    Put tracing activity in a using scope to help trigger export before program exit

--- examples/NewRelic.OpenTelemetry/ConsoleCore/Program.cs
+++ examples/NewRelic.OpenTelemetry/ConsoleCore/Program.cs
@@ -32,7 +32,7 @@ namespace SampleConsoleCoreApp
                 }
             );
 
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            using (var tracerProvider = Sdk.CreateTracerProviderBuilder()
                 .AddSource(ActivitySourceName)
                 .AddNewRelicExporter(options =>
                 {
@@ -41,22 +41,23 @@ namespace SampleConsoleCoreApp
                     options.AuditLoggingEnabled = true;
                 }, loggerFactory)
                 .AddHttpClientInstrumentation()
-                .Build();
-
-            using (var activity = SampleActivitySource.StartActivity("SampleConsoleCoreAppSpan"))
+                .Build())
             {
-                Console.WriteLine("Creating root of trace.");
+                using (var activity = SampleActivitySource.StartActivity("SampleConsoleCoreAppSpan"))
+                {
+                    Console.WriteLine("Creating root of trace.");
 
-                var message = "Hello, OpenTelemetry with New Relic!";
-                activity?.SetTag("aNumber", 42);
-                activity?.SetTag("message", message);
+                    var message = "Hello, OpenTelemetry with New Relic!";
+                    activity?.SetTag("aNumber", 42);
+                    activity?.SetTag("message", message);
 
-                Console.WriteLine("\nMaking an external HTTP request which will be added as a child span of the trace.");
+                    Console.WriteLine("\nMaking an external HTTP request which will be added as a child span of the trace.");
 
-                var httpClient = new HttpClient();
-                var request = httpClient.GetAsync("https://www.newrelic.com");
+                    var httpClient = new HttpClient();
+                    var request = httpClient.GetAsync("https://www.newrelic.com");
 
-                Console.WriteLine($"Web request result: {request.Result.StatusCode}");
+                    Console.WriteLine($"Web request result: {request.Result.StatusCode}");
+                }
             }
 
             Console.WriteLine("\nFinished.");
