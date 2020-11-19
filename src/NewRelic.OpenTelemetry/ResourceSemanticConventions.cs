efe755091b5188e93fb4845747502b097049bab3
Alan West <3676547+alanwest@users.noreply.github.com>
2020-11-19 09:05:55 -0800

    Update to OpenTelemetry 1.0.0-rc1.1 (#167)

---
+++ src/NewRelic.OpenTelemetry/ResourceSemanticConventions.cs
@@ -0,0 +1,11 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+namespace NewRelic.OpenTelemetry
+{
+    internal static class ResourceSemanticConventions
+    {
+        public const string AttributeServiceName = "service.name";
+        public const string AttributeServiceNamespace = "service.namespace";
+    }
+}
