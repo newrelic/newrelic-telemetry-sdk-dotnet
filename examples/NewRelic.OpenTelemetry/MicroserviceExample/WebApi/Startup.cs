e0e6a62f936a901789cb04d0ba57e60a210a09f3
Alex Hemsath <ahemsath@newrelic.com>
2020-11-20 09:57:02 -0800

    Add NR copyright notice to the files we modified

--- examples/NewRelic.OpenTelemetry/MicroserviceExample/WebApi/Startup.cs
+++ examples/NewRelic.OpenTelemetry/MicroserviceExample/WebApi/Startup.cs
@@ -1,5 +1,6 @@
 // <copyright file="Startup.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
+// Copyright 2020 New Relic, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
