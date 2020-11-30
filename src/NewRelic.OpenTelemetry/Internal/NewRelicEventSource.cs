3a76012cd5423779e1f03d6c6388188d173a77ef
Chris Ventura <45495992+nrcventura@users.noreply.github.com>
2020-11-30 14:05:03 -0800

    Log exceptions using consistent culture

--- src/NewRelic.OpenTelemetry/Internal/NewRelicEventSource.cs
+++ src/NewRelic.OpenTelemetry/Internal/NewRelicEventSource.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Threading;
 
 namespace NewRelic.OpenTelemetry.Internal
 {
@@ -18,7 +20,7 @@ namespace NewRelic.OpenTelemetry.Internal
             {
                 if (exception != null)
                 {
-                    EmitInformationalMessage(message, exception.ToString());
+                    EmitInformationalMessage(message, ToInvariantString(exception));
                 }
                 else
                 {
@@ -34,7 +36,7 @@ namespace NewRelic.OpenTelemetry.Internal
             {
                 if (exception != null)
                 {
-                    EmitErrorMessage(message, exception.ToString());
+                    EmitErrorMessage(message, ToInvariantString(exception));
                 }
                 else
                 {
@@ -48,7 +50,7 @@ namespace NewRelic.OpenTelemetry.Internal
         {
             if (IsEnabled(EventLevel.Error, (EventKeywords)(-1)))
             {
-                EmitErrorMessage(exception.ToString());
+                EmitErrorMessage(ToInvariantString(exception));
             }
         }
 
@@ -59,7 +61,7 @@ namespace NewRelic.OpenTelemetry.Internal
             {
                 if (exception != null)
                 {
-                    EmitInformationalMessage(message, exception.ToString());
+                    EmitInformationalMessage(message, ToInvariantString(exception));
                 }
                 else
                 {
@@ -75,7 +77,7 @@ namespace NewRelic.OpenTelemetry.Internal
             {
                 if (exception != null)
                 {
-                    EmitWarningMessage(message, exception.ToString());
+                    EmitWarningMessage(message, ToInvariantString(exception));
                 }
                 else
                 {
@@ -118,6 +120,25 @@ namespace NewRelic.OpenTelemetry.Internal
         public void EmitErrorMessage(string message, string error)
         {
             WriteEvent(1, message, error);
+        }
+
+        // Formats the exception using the same logic that is used within the OTel SDK eventsource
+        // loggers so that the log messages can be normalized to use the same culture information
+        // as the rest of the log messages. For a more detailed explanation refer to the following link.
+        // https://github.com/open-telemetry/opentelemetry-dotnet/pull/944#discussion_r462467096
+        private static string ToInvariantString(Exception exception)
+        {
+            var originalUICulture = Thread.CurrentThread.CurrentUICulture;
+
+            try
+            {
+                Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+                return exception.ToString();
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentUICulture = originalUICulture;
+            }
         }
     }
 }
